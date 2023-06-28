51621f969567862c762dde109f23135a7134caa9
Patrick Arriola <arriolapat101@gmail.com>
2023-06-27 19:08:54 -0700

    inital commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="bg-purple-900 h-full w-auto">
+	<div class="index-header bg-black mx-[294px] p-[100px]">
+		<div class="bg-green-500 flex flex-col justify-center items-center">
+			<h1 class="text-5xl"> Get measureable results from online marketing </h1>
+			<p class="text-xl">
+				"Do SEO, content marketing, competitor research,"
+				<br>
+				"PCC and social media marketing from just one platform"
+			</p>
+		</div>
+	</div>
+</div>
