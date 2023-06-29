958595b34e3afd95556f24b7bdc14e96d6f8fa17
Patrick Arriola <arriolapat101@gmail.com>
2023-06-28 19:07:31 -0700

    Components1.0

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,35 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<div class="bg-purple-900 h-full w-auto">
-	<div class="index-header bg-black mx-[294px] p-[100px]">
-		<div class="bg-green-500 flex flex-col justify-center items-center">
-			<h1 class="text-5xl"> Get measureable results from online marketing </h1>
-			<p class="text-xl">
-				"Do SEO, content marketing, competitor research,"
-				<br>
-				"PCC and social media marketing from just one platform"
-			</p>
+<div class="w-full flex items-center flex-col">
+
+	<div id="gradient" class="w-full bg-purple-800 flex justify-center pb-[10rem] ">
+		<div class="w-full max-w-[1200px]"> 
+			<div class="index-header bg-black p-[100px]">
+				<div class="bg-green-500 flex flex-col justify-center items-center">
+					<h1 class="text-5xl"> Get measureable results from online marketing </h1>
+					<p class="text-xl">
+						"Do SEO, content marketing, competitor research,"
+						<br>
+						"PCC and social media marketing from just one platform"
+					</p>
+				</div>
+			</div>
 		</div>
 	</div>
+
+	<div class="w-full max-w-[1200px] m-[-8rem]"> 
+		<div class="card h-[20rem] shadow-xl">
+			<img class="w-[300px] h-[200px] object-cover" src="https://placehold.co/300x200" alt="placeholder"/>
+		</div>
+		
+	</div>
 </div>
+
+<style lang="postcss">
+	#gradient {
+		background-image:
+		radial-gradient(at 0% 0%, rgba(var(--color-secondary-500) / 0.33) 0px, transparent 50%),
+		radial-gradient(at 98% 1%, rgba(var(--color-error-500) / 0.33) 0px, transparent 50%);
+	}
+
+</style>
